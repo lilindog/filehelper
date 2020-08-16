dd03b93fe269a5e1b7242019131238da55988f2c
lilindog <lilin@lilin.site>
2020-08-16 20:47:19 +0800

    增加auth事件
    补充上次，删除out.json

--- example/index.js
+++ example/index.js
@@ -4,9 +4,18 @@ const fs = require("fs");
 
 let tree = new Tree();
 !async function () {
+    let authNodes = [];
     tree.on("step", treeNode => {
         console.log(treeNode.name);
     });
-    const res = await tree.parse({dir: "../../sdtree", excludes: [".git", "node_modules"]});
+    tree.on("auth", treeNode => {
+        authNodes.push(treeNode);
+        console.log("遇到了需要权限的：");
+        console.log(treeNode);
+        process.exit();
+    });
+    const res = await tree.parse({dir: "../../../", excludes: [".git", "node_modules"]});
     fs.writeFileSync("./out.json", JSON.stringify(res, null, 4));
+    console.log("没有权限的有：");
+    console.log(authNodes);
 }();
