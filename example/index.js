53b9f962d8067eae25b4f6afb45282fdaf3077f6
lilindog <lilin@lilin.site>
2020-08-31 11:40:19 +0800

    重写Scan, 可以支持循环暂停,改用了while循环实现的深度优先遍历

--- example/index.js
+++ example/index.js
@@ -1,21 +1,20 @@
 const Scan = require("../lib/Scan");
-const Tree = require("../lib/Tree");
-const fs = require("fs");
+const path = require("path");
 
-let tree = new Tree();
-!async function () {
-    let authNodes = [];
-    tree.on("step", treeNode => {
-        console.log(treeNode.name);
-    });
-    tree.on("auth", treeNode => {
-        authNodes.push(treeNode);
-        console.log("遇到了需要权限的：");
-        console.log(treeNode);
-        process.exit();
-    });
-    const res = await tree.parse({dir: "../../../", excludes: [".git", "node_modules"]});
-    fs.writeFileSync("./out.json", JSON.stringify(res, null, 4));
-    console.log("没有权限的有：");
-    console.log(authNodes);
-}();
+let scan = new Scan({dir: "../../../", authSkip: !true});
+scan.on("data", node => {
+    console.log(node.name);
+});
+scan.on("auth", node => {
+    console.log("无权限");
+    console.log(node);
+    setTimeout(() => {
+        console.log(">> 继续");
+        scan.resume();
+    }, 1000);
+});
+scan.on("done", () => {
+    console.log("!! 扫描 完啦");
+});
+
+scan.parse();
